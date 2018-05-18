de332aa3c08f773453950e09120d5dfc226d0b2a
Alejandro Sabogal <asabogal.git@gmail.com>
2018-05-18 02:53:15 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -11,7 +11,8 @@ class Student
   end
 
   def self.create_from_collection(students_array)
-    students_array.each {|hash| binding.pry Student.new(hash)}
+    students_array.each {|hash| Student.new(hash)}
+    binding.pry
   end
 
   def add_student_attributes(attributes_hash)
